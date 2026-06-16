4a933cf66458c196f61ec0f4117730679a6a8626
Humphrey <a739376838@gmail.com>
2026-06-16 17:25:42 -0500

    Add: palindrome-partitioning - submission-3

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        path = []
+
+
+        def bt(start):
+            if start == len(s):
+                res.append(path[:])
+
+                return
+
+            for end in range(start + 1, len(s) + 1):
+                sub =s[start : end]
+
+                if sub == sub[::-1]:
+                    path.append(sub)
+                    bt(end)
+                    path.pop()
+
+
+
+        bt(0)
+        return res
+
+
+
+        
