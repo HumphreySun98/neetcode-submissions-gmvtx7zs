3bd7f2355273508e96f38d04816183c04b0c2591
Humphrey <a739376838@gmail.com>
2026-07-01 17:07:22 -0500

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,37 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        indegree = [0] * numCourses
+
+        for a,b in prerequisites:
+            graph[b].append(a)
+            indegree[a] += 1
+
+        queue = deque()
+
+        for course in range(numCourses):
+            if indegree[course] == 0:
+                queue.append(course)
+
+        finished = 0
+        
+        while queue:
+            course = queue.popleft()
+            finished +=1 
+
+            for next_course in graph[course]:
+                indegree[next_course] -= 1
+
+
+                if indegree[next_course] == 0:
+                    queue.append(next_course)
+
+
+
+        if finished == numCourses:
+            return True
+
+        else:
+            return False
+
+        
