faa9d2111410767eb7df8a0490fb263e79ecd39d
Humphrey <a739376838@gmail.com>
2026-08-24 20:25:45 -0400

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        end = 0
+        jumps = 0
+        far = 0
+
+
+        for i in range(len(nums)-1):
+            far = max(far,i+nums[i])
+            if i == end:
+                jumps += 1
+                end = far
+
+
+        return jumps
+
+        
